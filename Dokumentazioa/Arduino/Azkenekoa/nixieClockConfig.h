617d8a83f62480e734b8efd2e9c0f809b6f92bcc
FakeNixie <iker.alonso04@ikasle.db.eus>
2025-02-19 10:20:41 +0100

    Add files via upload

---
+++ Dokumentazioa/Arduino/Azkenekoa/nixieClockConfig.h
@@ -0,0 +1,7 @@
+//Define your SSID and Password here
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+//Define the Daylight Saving rule here
+#define DST_RULE ""
+//the rules are available here https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
